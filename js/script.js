9871a7365f2bcfdb4d0112b0953c72eaceda2678
Javiuty <javiiescuadra@gmail.com>
2020-11-30 17:40:33 +0100

    renderizando objetos de la bbdd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Variables
+let container = document.querySelector(".canciones__main-container");
+
 // Fetching canciones from our API
 function callApi() {
   fetch("http://localhost:5000/canciones")
@@ -10,7 +13,39 @@ function callApi() {
 }
 callApi();
 
-// Renderizar los objetos del array
+// Sacando url, id del link
 function handlingLinks(links) {
-  links.forEach((link) => console.log(link));
+  links.forEach((link) => {
+    const url = link.link;
+
+    const id = url.slice(32, 43);
+
+    callingApiYoutube(id);
+  });
+}
+
+//
+function callingApiYoutube(id) {
+  fetch(
+    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${GOOGLE_KEY.apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => renderingHTML(data));
+}
+
+function renderingHTML(video) {
+  const titulo = video.items[0].snippet.title;
+  const imagenVideo = video.items[0].snippet.thumbnails.high.url;
+
+  let div = document.createElement("div");
+  div.classList.add("canciones__cancion");
+  div.innerHTML = `
+  <img src=${imagenVideo} alt=${titulo} />
+  <div class="canciones__cancion-title">
+    <h3>${titulo}</h3>
+  </div>
+  <button class="btn">Descargar</button>
+  `;
+
+  container.appendChild(div);
 }
