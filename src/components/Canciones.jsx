57ccadbe91fa0748cb3b49e037f8de036f0d185b
Javiuty <javiiescuadra@gmail.com>
2021-02-25 20:11:08 +0100

    lint update

--- src/components/Canciones.jsx
+++ src/components/Canciones.jsx
@@ -1,46 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import axios from "axios";
 
 const Canciones = () => {
+  const [songs, setSongs] = useState([]);
 
-    const [songs, setSongs] = useState([])
+  useEffect(() => {
+    const callingApi = async () => {
+      const url = "https://whispering-tundra-59051.herokuapp.com/canciones";
 
-    useEffect(() => {
+      const resultado = await axios.get(url);
 
-        const callingApi = async () => {
-            const url = 'https://whispering-tundra-59051.herokuapp.com/canciones'
+      setSongs(resultado.data);
 
-            const resultado = await axios.get(url)
+      handlingLinks(songs);
+    };
+    callingApi();
+  }, []);
 
-            setSongs(resultado.data)
+  const handlingLinks = (canciones) => {
+    canciones.forEach((cancion) => {
+      const url = cancion.link;
 
-            handlingLinks(songs)
-        }
-        callingApi()
-       
-    },[])
+      const id = url.slice(32, 43);
 
-    const handlingLinks = canciones => {
-        canciones.forEach((cancion) => {
-          const url = cancion.link;
-      
-          const id = url.slice(32, 43);
-      
-          const hora = parseFloat(cancion.fecha);
-      
-          const horaFormateada = moment(hora).locale("es").calendar();
-      
-          /* callingApiYoutube(id, horaFormateada); */
-        });
-      }
+      const hora = parseFloat(cancion.fecha);
 
+      const horaFormateada = moment(hora).locale("es").calendar();
 
+      /* callingApiYoutube(id, horaFormateada); */
+    });
+  };
 
-    return (
-        <>
-            <h2>Canciones</h2>
-        </>
-    )
-}
- 
+  return (
+    <>
+      <h2>Canciones</h2>
+    </>
+  );
+};
+
 export default Canciones;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,72 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import Error from './Error'
+import React, { useState } from "react";
+import Error from "./Error";
 
 const Form = ({ setUrl }) => {
+  const [inputSong, setInputSong] = useState("");
+  const [error, setError] = useState(false);
+  const [exito, setExito] = useState(false);
 
-    const [inputSong, setInputSong] = useState('')
-    const [error, setError] = useState(false);
-    const [exito, setExito] = useState(false);
+  const handlingForm = async (e) => {
+    e.preventDefault();
 
-    const handlingForm = async e => {
-        e.preventDefault();
-
-      if (inputSong === "" ||
+    if (
+      inputSong === "" ||
       !inputSong.includes("https://www.youtube.com") ||
-      !inputSong.includes("v=")) {
-
-        setError(true)
-        return null;
-      } 
-      setError(false)
-      /* Hacemos petición Fetch para enviar datos a nuestro server */
-      await fetch(
-        "https://whispering-tundra-59051.herokuapp.com/agregar-cancion",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            link: inputSong,
-            fecha: Date.now(),
-          }),
-        }
-      ); 
-
-      setExito(true)
-
-      setTimeout(()=> {
-        setExito(false)
-      }, 3000)
-
-      /* Reseteamos form y estado */
-      setInputSong('')
-      document.getElementById('enviar-form').reset()
-     
-
+      !inputSong.includes("v=")
+    ) {
+      setError(true);
+      return null;
     }
+    setError(false);
+    /* Hacemos petición Fetch para enviar datos a nuestro server */
+    await fetch(
+      "https://whispering-tundra-59051.herokuapp.com/agregar-cancion",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          link: inputSong,
+          fecha: Date.now(),
+        }),
+      }
+    );
 
-    return ( 
-     
-        <form id="enviar-form" onSubmit={handlingForm}>
-             { error ? <Error mensaje="No es un enlace válido" tipo="error" /> : null}
-             { exito ? <Error mensaje="Link enviado con éxito" /> : null}
-            <div className="header__form-container">
-              <label>
-                <input
-                  id="buscador"
-                  type="text"
-                  placeholder="Inserta aquí el link de youtube"
-                  onChange={e => setInputSong(e.target.value.trim())}
-                />
-              </label>
-              <button type="submit" id="addBtn" className="btn">
-                Añadir canción
-              </button>
-            </div>
-          </form>
-     );
-}
- 
+    setExito(true);
+
+    setTimeout(() => {
+      setExito(false);
+    }, 3000);
+
+    /* Reseteamos form y estado */
+    setInputSong("");
+    document.getElementById("enviar-form").reset();
+  };
+
+  return (
+    <form id="enviar-form" onSubmit={handlingForm}>
+      {error ? <Error mensaje="No es un enlace válido" tipo="error" /> : null}
+      {exito ? <Error mensaje="Link enviado con éxito" /> : null}
+      <div className="header__form-container">
+        <label>
+          <input
+            id="buscador"
+            type="text"
+            placeholder="Inserta aquí el link de youtube"
+            onChange={(e) => setInputSong(e.target.value.trim())}
+          />
+        </label>
+        <button type="submit" id="addBtn" className="btn">
+          Añadir canción
+        </button>
+      </div>
+    </form>
+  );
+};
+
 export default Form;
